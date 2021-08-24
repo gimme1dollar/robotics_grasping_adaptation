fd2cb1c700686e13b7e45c0ecb3a991ab94e4e01
JYLee <gimme1dollar@postech.ac.kr>
2021-08-24 20:24:33 +0900

    random test

---
+++ main.py
@@ -0,0 +1,37 @@
+import pybullet as p
+import numpy as np
+import os
+from tqdm import tqdm
+from itertools import count
+
+import gym
+import agent
+from agent.common import io_utils
+
+config = io_utils.load_yaml("config/gripper_grasp.yaml")
+env = gym.make("gripper-env-v0", config=config)
+total_timestep=100_000
+
+img_h, img_w, img_c = 64, 64, 3
+action_shape = env.action_space.shape
+action_min = env.action_space.low
+action_max = env.action_space.high
+
+last_obs = env.reset()
+with tqdm(total=total_timestep) as pbar:
+    for t in count():
+        # update progress bar
+        pbar.n = t
+        pbar.refresh()
+
+        if t >= total_timestep:
+            break
+
+        action = np.random.rand(5)
+        action = action * (action_max - action_min) + action_min
+        obs, reward, done, _ = env.step(action)
+        
+        if done:
+            obs = env.reset()
+            #print(reward)
+        last_obs = obs
